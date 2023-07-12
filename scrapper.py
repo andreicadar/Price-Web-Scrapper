8497e918dde1eb81ebba7366050c9323a7c7c231
Andrei Cadar <cadarandrei2370@yahoo.com>
2023-07-12 22:14:03 +0300

    update

--- scrapper.py
+++ scrapper.py
@@ -12,27 +12,25 @@ import time
 from itertools import product
 
 products = {"entries":
-            [{"name": "Samsung S23 Plus",
-              "Altex": "https://altex.ro/telefon-samsung-galaxy-s23-5g-256gb-8gb-ram-dual-sim-phantom-black/cpd/SMTS23P2BK/",
-              "MediaGalaxy": "https://mediagalaxy.ro/telefon-samsung-galaxy-s23-5g-256gb-8gb-ram-dual-sim-phantom-black/cpd/SMTS23P2BK/",
-              "Emag": "https://www.emag.ro/telefon-mobil-samsung-galaxy-s23-plus-dual-sim-8gb-ram-256gb-5g-phantom-black-sm-s916bzkdeue/pd/DB7R8RMBM/",
-              "PCGarage": "https://www.pcgarage.ro/smartphone/samsung/galaxy-s23-plus-octa-core-256gb-8gb-ram-dual-sim-5g-4-camere-phantom-black/"},
-             {"name": "Samsung S23 Ultra",
-              "Altex": "https://altex.ro/telefon-samsung-galaxy-s23-ultra-5g-256gb-8gb-ram-dual-sim-phantom-black/cpd/SMTS23U2BK/",
-              "MediaGalaxy": "https://mediagalaxy.ro/telefon-samsung-galaxy-s23-ultra-5g-256gb-8gb-ram-dual-sim-phantom-black/cpd/SMTS23U2BK/",
-              "Emag": "https://www.emag.ro/telefon-mobil-samsung-galaxy-s23-ultra-dual-sim-8gb-ram-256gb-5g-phantom-black-sm-s918bzkdeue/pd/D07R8RMBM/",
-              "PCGarage": "https://www.pcgarage.ro/smartphone/samsung/galaxy-s23-ultra-octa-core-256gb-8gb-ram-dual-sim-5g-5-camere-phantom-black/"},
-             {"name": "Mouse Razer V2 DeathAdder HyperSpeed",
-              "Altex": "https://altex.ro/mouse-gaming-wireless-razer-deathadder-v2-x-hyperspeed-dual-mode-14000-dpi-bluetooth-negru/cpd/MOURZ014131R3G1/",
-              "MediaGalaxy": "https://mediagalaxy.ro/mouse-gaming-wireless-razer-deathadder-v2-x-hyperspeed-dual-mode-14000-dpi-bluetooth-negru/cpd/MOURZ014131R3G1/",
-              "Emag": "https://www.emag.ro/mouse-gaming-razer-deathadder-v2-x-hyperspeed-wireless-14k-dpi-negru-rz01-04130100-r3g1/pd/D2XR99MBM/",
-              "PCGarage": "https://www.pcgarage.ro/mouse-gaming/razer/deathadder-v2-x-hyperspeed-wireles-black/"}
-             ]}
+                [{"name": "Samsung S23 Plus",
+                  "Altex": "https://altex.ro/telefon-samsung-galaxy-s23-5g-256gb-8gb-ram-dual-sim-phantom-black/cpd/SMTS23P2BK/",
+                  "MediaGalaxy": "https://mediagalaxy.ro/telefon-samsung-galaxy-s23-5g-256gb-8gb-ram-dual-sim-phantom-black/cpd/SMTS23P2BK/",
+                  "Emag": "https://www.emag.ro/telefon-mobil-samsung-galaxy-s23-plus-dual-sim-8gb-ram-256gb-5g-phantom-black-sm-s916bzkdeue/pd/DB7R8RMBM/",
+                  "PCGarage": "https://www.pcgarage.ro/smartphone/samsung/galaxy-s23-plus-octa-core-256gb-8gb-ram-dual-sim-5g-4-camere-phantom-black/"},
+                 {"name": "Samsung S23 Ultra",
+                  "Altex": "https://altex.ro/telefon-samsung-galaxy-s23-ultra-5g-256gb-8gb-ram-dual-sim-phantom-black/cpd/SMTS23U2BK/",
+                  "MediaGalaxy": "https://mediagalaxy.ro/telefon-samsung-galaxy-s23-ultra-5g-256gb-8gb-ram-dual-sim-phantom-black/cpd/SMTS23U2BK/",
+                  "Emag": "https://www.emag.ro/telefon-mobil-samsung-galaxy-s23-ultra-dual-sim-8gb-ram-256gb-5g-phantom-black-sm-s918bzkdeue/pd/D07R8RMBM/",
+                  "PCGarage": "https://www.pcgarage.ro/smartphone/samsung/galaxy-s23-ultra-octa-core-256gb-8gb-ram-dual-sim-5g-5-camere-phantom-black/"},
+                 {"name": "Mouse Razer V2 DeathAdder HyperSpeed",
+                  "Altex": "https://altex.ro/mouse-gaming-wireless-razer-deathadder-v2-x-hyperspeed-dual-mode-14000-dpi-bluetooth-negru/cpd/MOURZ014131R3G1/",
+                  "MediaGalaxy": "https://mediagalaxy.ro/mouse-gaming-wireless-razer-deathadder-v2-x-hyperspeed-dual-mode-14000-dpi-bluetooth-negru/cpd/MOURZ014131R3G1/",
+                  "Emag": "https://www.emag.ro/mouse-gaming-razer-deathadder-v2-x-hyperspeed-wireless-14k-dpi-negru-rz01-04130100-r3g1/pd/D2XR99MBM/",
+                  "PCGarage": "https://www.pcgarage.ro/mouse-gaming/razer/deathadder-v2-x-hyperspeed-wireles-black/"}
+                 ]}
 
-# pathToAddForLinux = ""
+#pathToAddForLinux = ""
 pathToAddForLinux = "/home/andrei/ownCloud - owncloud@172.24.12.252/"
-
-
 def search_price(item_name, website):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
@@ -102,8 +100,7 @@ def search_price(item_name, website):
 
 
 def do_plot(productName, shopName):
-    data = pd.read_csv(pathToAddForLinux + productName +
-                       "/" + shopName + ".txt")
+    data = pd.read_csv(pathToAddForLinux + productName + "/" + shopName + ".txt")
 
     dates = [dt.datetime.strptime(d, '%d/%m/%Y').date() for d in data["Date"]]
     fig, ax = plt.subplots(figsize=(20, 10))
@@ -115,8 +112,7 @@ def do_plot(productName, shopName):
     plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
 
     # plt.show()
-    plt.savefig(pathToAddForLinux + productName +
-                "/" + shopName + "_PriceGraph.png")
+    plt.savefig(pathToAddForLinux + productName + "/" + shopName + "_PriceGraph.png")
 
 
 def replacer(s, newstring, index, nofail=False):
@@ -140,7 +136,7 @@ def replace_space(string, strings, currentIndex):
     if occurance == -1:
         strings.append(string)
     else:
-        copy = string
+        copy = string;
         copy = replacer(copy, '/', occurance)
         replace_space(copy, strings, currentIndex)
         string = replacer(string, '-', occurance)
@@ -164,17 +160,30 @@ def replace_space(string, strings, currentIndex):
 # search_price(altex_name, altex_name)
 # search_price(mediaGalaxy_name, "https://mediagalaxy.ro/"+altex_name);
 
-# def PC_Garage_method(strings):
-#     found = 0;
-#     for string in strings:
-#         result = search_price(string, "https://www.pcgarage.ro/" + string)
-#         if result != -1:
-#             print(f"The price of {item_name} is {result} on https://www.pcgarage.ro/{string}")
-#             found = 1
-#             break
-#     if found == 0:
-#         print("nu s a gasit pe" + "https://www.pcgarage.ro/")
+def PC_Garage_method(strings):
+    found = 0;
+    for string in strings:
+        result = search_price(string, "https://www.pcgarage.ro/" + string)
+        if result != -1:
+            print(f"The price of {item_name} is {result} on https://www.pcgarage.ro/{string}")
+            found = 1
+            break
+    if found == 0:
+        print("nu s a gasit pe" + "https://www.pcgarage.ro/")
 
+
+# print(item_name)
+# strings = []
+# replace_space(item_name, strings, 0)
+# print(strings)
+# PC_Garage_method(strings)
+
+
+# item_name = "S23 ultra"
+# search_price(item_name, "https://www.emag.ro/telefon-mobil-samsung-galaxy-s23-ultra-dual-sim-8gb-ram-256gb-5g-phantom-black-sm-s918bzkdeue/pd/D07R8RMBM/?_gl=1*mib327*_up*MQ..&gclid=CjwKCAjwuqiiBhBtEiwATgvixC8siMkJ6xt_J3Pv3O6b_s5Tm3OSntUE80vSbblHJLB8leehKHo2aRoCNPMQAvD_BwE")
+#
+# item_name = "S23 plus"
+# search_price(item_name, "https://www.emag.ro/telefon-mobil-samsung-galaxy-s23-plus-dual-sim-8gb-ram-256gb-5g-phantom-black-sm-s916bzkdeue/pd/DB7R8RMBM/")
 
 products_dict = json.loads(json.dumps(products))
 entries = products_dict['entries']
@@ -191,9 +200,9 @@ for entry in entries:
 
 for entry in entries:
     time.sleep(5)
-    do_plot(entry["name"], "Altex")
-    do_plot(entry["name"], "MediaGalaxy")
-    do_plot(entry["name"], "Emag")
-    do_plot(entry["name"], "PCGarage")
+    do_plot(entry["name"], "Altex");
+    do_plot(entry["name"], "MediaGalaxy");
+    do_plot(entry["name"], "Emag");
+    do_plot(entry["name"], "PCGarage");
 
 # subprocess.run([sys.executable, pathToAddForLinux + "graphPlotter.py", pathToAddForLinux, products])
